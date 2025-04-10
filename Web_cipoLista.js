e0640ed109085d6bc4f1cb143b134ecb6dc6609d
Tordai Levente Barnabás (Szf_N_2024_2026) <tordai.levente.barnabas@diak.szamalk-szalezi.hu>
2025-04-10 12:03:42 +0200

    rendezget4esessseg

--- Web_cipoLista.js
+++ Web_cipoLista.js
@@ -4,7 +4,8 @@ export default class Web_cipoLista {
         { nev: "Adidas Campus", ar: 28000, kep: "kepek/adidascipo.jpg", meret: 41 },
         { nev: "Jordan 6", ar: 31000, kep: "kepek/jordan6.jpg", meret: 43 },
         { nev: "Jordan Klumpa", ar: 31000, kep: "kepek/jordanKlumpa.jpg", meret: 43 },
-        { nev: "Nike Air Force 1", ar: 31000, kep: "kepek/nikeairforce1.jpg", meret: 43 }
+        { nev: "Nike Air Force 1", ar: 31000, kep: "kepek/nikeairforce1.jpg", meret: 43 },
+        { nev: "Nike Python", ar: 78000, kep: "kepek/nikeKigyobor.jpg", meret: 44 }
     ];
 
     getLista() {
