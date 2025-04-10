e1cf7de46e439e930543d363f30eb01c99ac1493
S4mur419020 <muraitomi04@gmail.com>
2025-04-10 10:59:44 +0200

    kepek

--- Web_cipoLista.js
+++ Web_cipoLista.js
@@ -1,7 +1,7 @@
 export default class Web_cipoLista {
     #lista = [
-        { nev: "Nike Air Max", ar: 32000, kep: "kepek/nike.jpg", meret: 42 },
-        { nev: "Adidas Superstar", ar: 28000, kep: "kepek/adidas.jpg", meret: 41 },
+        { nev: "Nike Air Force 1", ar: 32000, kep: "kepek/nikeairforce1.jpg", meret: 42 },
+        { nev: "Adidas Campus", ar: 28000, kep: "kepek/adidascipo.jpg", meret: 41 },
         { nev: "Puma RS-X", ar: 31000, kep: "kepek/puma.jpg", meret: 43 }
     ];
 
