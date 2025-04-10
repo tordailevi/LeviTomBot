fd3312fab1eb0674ea675db006731a5b6c6ac2fe
S4mur419020 <muraitomi04@gmail.com>
2025-04-10 11:07:18 +0200

    cipok

--- Web_cipoLista.js
+++ Web_cipoLista.js
@@ -1,8 +1,10 @@
 export default class Web_cipoLista {
     #lista = [
-        { nev: "Nike Air Force 1", ar: 32000, kep: "kepek/nikeairforce1.jpg", meret: 42 },
+        { nev: "Jordan 4", ar: 32000, kep: "kepek/jordan4.jpg", meret: 42 },
         { nev: "Adidas Campus", ar: 28000, kep: "kepek/adidascipo.jpg", meret: 41 },
-        { nev: "Puma RS-X", ar: 31000, kep: "kepek/puma.jpg", meret: 43 }
+        { nev: "Jordan 6", ar: 31000, kep: "kepek/jordan6.jpg", meret: 43 },
+        { nev: "Jordan Klumpa", ar: 31000, kep: "kepek/jordanKlumpa.jpg", meret: 43 },
+        { nev: "Nike Air Force 1", ar: 31000, kep: "kepek/nikeairforce1.jpg", meret: 43 }
     ];
 
     getLista() {
